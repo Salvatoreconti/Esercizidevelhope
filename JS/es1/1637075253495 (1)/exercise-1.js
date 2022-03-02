5a44eb9ad07e48d09e234fd245ca5efafbe7065e
Salvatore <salvatoreconti713@gmail.com>
2022-03-02 17:13:46 +0100

    modify es1

--- JS/es1/1637075253495 (1)/exercise-1.js
+++ JS/es1/1637075253495 (1)/exercise-1.js
@@ -1,11 +1,11 @@
 function canPlay() {
   if (true) {
-    let personName = 'Paul';
+    
   }
 
-  personName += ' plays football'; // Argh! personName is not defined
-
-  console.log(personName);
 }
+let personName = 'Paul';
+personName += ' plays football';
+console.log(personName);
 
 canPlay();
